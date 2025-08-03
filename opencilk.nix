3e5db71d026c1a62ab355b5d22345860398dc40f
Aniruddh Srivastava <noirrr@duck.com>
2025-08-03 14:31:49 +0000

    New attempt to package Tried using cc-wrapper https://github.com/NixOS/nixpkgs/tree/25.05/pkgs/build-support/cc-wrapper but failed with a similar error message

--- opencilk.nix
+++ opencilk.nix
@@ -2,27 +2,23 @@
 
 let
   gcc14Stdenv = pkgs.overrideCC pkgs.stdenv pkgs.gcc14;
-
-  inherit (pkgs) fetchgit;
+  inherit (pkgs) fetchgit cmake ninja git;
 
   infrastructureSrc = fetchgit {
     url    = "https://github.com/OpenCilk/infrastructure.git";
     rev    = "opencilk/v3.0";
     sha256 = "dpBiCPT0ZwKLi1ezD06/hVG40QW9azunqXPxwn0gDO4=";
   };
-
   opencilkProjectSrc = fetchgit {
     url    = "https://github.com/OpenCilk/opencilk-project.git";
     rev    = "opencilk/v3.0";
     sha256 = "QIBpCsdDVbXh35VGVDymI9CvAnTo1SA0UxZvJ0LUy0k=";
   };
-
   cheetahSrc = fetchgit {
     url    = "https://github.com/OpenCilk/cheetah.git";
     rev    = "opencilk/v3.0";
     sha256 = "qBjYuCqw7Dk3IFrf9nEkGMjdveev2WiX9zSDHoSpnDc=";
   };
-
   productivityToolsSrc = fetchgit {
     url    = "https://github.com/OpenCilk/productivity-tools.git";
     rev    = "opencilk/v3.0";
@@ -30,30 +26,21 @@ let
   };
 
   combinedSrc = pkgs.runCommand "combine-all-repos" { } ''
-    mkdir -p $out
-
-    mkdir -p $out/infrastructure
-    cp -r ${infrastructureSrc}/*  $out/infrastructure/
-
     mkdir -p $out/infrastructure/opencilk
     cp -r ${opencilkProjectSrc}/* $out/infrastructure/opencilk/
 
     mkdir -p $out/infrastructure/opencilk/cheetah
-    cp -r ${cheetahSrc}/*        $out/infrastructure/opencilk/cheetah/
+    cp -r ${cheetahSrc}/* $out/infrastructure/opencilk/cheetah/
 
     mkdir -p $out/infrastructure/opencilk/cilktools
     cp -r ${productivityToolsSrc}/* $out/infrastructure/opencilk/cilktools/
-
-    mkdir -p $out/infrastructure/build
-
   '';
 
-  myOpenCilk = gcc14Stdenv.mkDerivation rec {
-    pname    = "openCilk-from-source";
-    version  = "2.1";
-
-    # The top‐level src is “combinedSrc” (which contains infrastructure/ and its subfolders)
-    src = combinedSrc;
+  # build Clang (unwrapped)
+  clangUnwrapped = gcc14Stdenv.mkDerivation {
+    pname   = "opencilk-clang-unwrapped";
+    version = "v3.0";
+    src     = combinedSrc;
 
     nativeBuildInputs = [
       pkgs.git
@@ -79,10 +66,6 @@ let
       pkgs.llvmPackages.clang
       pkgs.llvmPackages_16.bintools
 
-      # pkgs.ocaml
-      # pkgs.ocamlPackages.findlib
-      # pkgs.ocamlPackages.ctypes
-
       pkgs.valgrind
 
       pkgs.ninja
@@ -93,27 +76,92 @@ let
     phases = [ "unpackPhase" "buildPhase" "installPhase" ];
 
     buildPhase = ''
-      set -ex
-
-      mkdir -p $PWD/build
-      BUILD_DIR=$PWD/build
-
-      export OPENCILK_INSTALL_PREFIX=$out
-
-      INFRA_DIR="$src/infrastructure"
-
-      OPENCILK_DIR="$src/infrastructure/opencilk"
-
-      sh $INFRA_DIR/tools/build $OPENCILK_DIR $BUILD_DIR
+      mkdir build && cd build
+      cmake -G Ninja \
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+        -DLLVM_TARGETS_TO_BUILD=host \
+        -DC_INCLUDE_DIRS=${pkgs.glibc.dev}/include \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$out \
+        $src/infrastructure/opencilk/llvm
+      cmake --build . --target clang -- -j 5
     '';
 
-    installPhase = ''
-      set -ex
-      cd $BUILD_DIR
-      cmake --install . --prefix $OPENCILK_INSTALL_PREFIX
-    '';
+    installPhase = "ninja install-clang";
   };
-in
 
-{ inherit myOpenCilk; }
+  # Wrap the compiler so it is hopefully Nix‑aware (cc‑wrapper)
+  clangWrapped = pkgs.wrapCCWith rec {
+    cc = clangUnwrapped;
+    bintools = pkgs.wrapBintoolsWith {
+      bintools = pkgs.binutils;
+      libc     = pkgs.glibc;
+    };
+    extraPackages = [ pkgs.gcc14.cc ];
+  };
+
+  clangStdenv = pkgs.overrideCC pkgs.stdenv clangWrapped;
+
+  # build the rest of OpenCilk
+  myOpenCilk = clangStdenv.mkDerivation rec {
+    pname   = "openCilk";
+    version = "v3.0";
+    src     = combinedSrc;
+
+    nativeBuildInputs = [
+      pkgs.git
+      
+      pkgs.zlib
+      pkgs.libxml2
+      pkgs.libffi
+      pkgs.libpfm
+      pkgs.libxcrypt
+
+      pkgs.jemalloc
+      pkgs.libunwind
+
+      pkgs.cmake
+      
+      pkgs.python3
+      pkgs.python3Packages.pygments
+      pkgs.python3Packages.pyyaml
+      
+      pkgs.ncurses
+      pkgs.libedit
+
+      pkgs.llvmPackages.clang
+      pkgs.llvmPackages_16.bintools
+
+      pkgs.valgrind
+
+      pkgs.ninja
+
+      pkgs.binutils
+
+      clangWrapped
+    ];
+
+    phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+
+    buildPhase = ''
+      mkdir build && cd build
+      cmake -G Ninja \
+        -DCMAKE_C_COMPILER=clang   \
+        -DCMAKE_CXX_COMPILER=clang++ \
+        -DCLANG_CONFIG_FILE_SYSTEM_DIR=$out/etc/clang \
+        -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
+        -DLLVM_ENABLE_RUNTIMES="cheetah;cilktools" \
+        -DLLVM_TARGETS_TO_BUILD=host \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=$out \
+        $src/infrastructure/opencilk/llvm
+      cmake --build . -- -j
+    '';
+    installPhase = "ninja install";
+  };
+
+in
+{
+  inherit myOpenCilk clangWrapped;
+}
 
